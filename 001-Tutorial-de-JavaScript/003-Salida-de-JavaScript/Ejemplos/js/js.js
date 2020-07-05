8ff5af0ffa166759d915fcbc1b42738cc5c6ed0c
codefuncode <codefuncode@protonmail.com>
2020-07-04 23:57:55 -0400

    003 en proseso , indice para git web creado

---
+++ 001-Tutorial-de-JavaScript/003-Salida-de-JavaScript/Ejemplos/js/js.js
@@ -0,0 +1,36 @@
+var btninput = document.createElement("input");
+var btnbutton = document.createElement("button");
+var atttype = document.createAttribute("type");
+
+atttype.value = "button";
+btninput.setAttributeNode(atttype);
+btninput.value = "input creado";
+btnbutton.innerHTML = "Boton creado";
+
+document.body.appendChild(btnbutton);
+document.body.appendChild(btninput);
+
+var botones = document.querySelectorAll("input");
+
+for (var i = 0; i < botones.length; i++) {
+    console.log(botones[i]);
+}
+
+// =======================================================
+// document.getElementById("demo").innerHTML = 5 + 6;
+
+// document.write(5 + 6);
+
+function ejecutaalerta(argument) {
+    window.alert(5 + 6);
+}
+
+// console.log(5 + 6);
+// window.print()
+
+function myFunction() {
+
+    var myWindow = window.open("", "MsgWindow", "width=300,height=300");
+    myWindow.document.write("<p> Escribimos un parafo dentro de una ventana nueva </p>");
+    myWindow.document.body.appendChild(btninput);
+}
