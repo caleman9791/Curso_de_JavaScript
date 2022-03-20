d85e919df50fbe3cbb1c426ebe250dbcf4e0b060
Carlos Aleman <caleman9791@gmail.com>
2022-03-20 16:57:25 -0400

    ok

---
+++ Archivo/Ejemplos/permutacio-combinacion/js.js
@@ -0,0 +1,70 @@
+let txt = document.querySelectorAll("input");
+let btn = document.querySelector("button");
+let resultado = document.querySelector(".resultado ol");
+
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+// ///////////////////////////////////////////////
+
+function allPermutations(items) {
+  // allPermutations () : return a list of all possible permutations
+  // Credits: https://stackoverflow.com/questions/9960908/permutations-in-javascript
+  // PARAM items : array of items
+
+  let results = [];
+
+  function permute(arr, memo) {
+    var cur, memo = memo || [];
+    for (let i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+    return results;
+  }
+  permute(items);
+  return results;
+}
+// ///////////////////////////////////////////////
+
+// var fruits = ["Apple", "Banana", "Coconut"];
+// var permutated = allPermutations(fruits);
+// console.log(permutated);
+
+
+btn.addEventListener("click", function(argument) {
+
+
+  let matriz = [];
+
+  for (var i = 0; i < txt.length; i++) {
+
+    if (txt[i].value.length >= 1) {
+      matriz.push(txt[i].value);
+    }
+
+  }
+
+  var permutated = allPermutations(matriz);
+
+
+  console.log(permutated);
+  let resultado = document.querySelector(".resultado ol");
+
+  removeAllChildNodes(resultado);
+  let lista = [];
+  for (var i = 0; i < permutated.length; i++) {
+    lista.push(document.createElement("li"));
+    let text = permutated[i].toString();
+    lista[i].textContent = text;
+    resultado.appendChild(lista[i]);
+  }
+
+});
---
+++ Archivo/devdocs/00001_array/00002/index.js
@@ -0,0 +1,64 @@
+const matriz = ['a', 'b', 'c'];
+
+const literable_matriz =
+  matriz[Symbol.iterator]();
+
+const resultado_caracter =
+  document.getElementById('resultado_caracter');
+
+// Su navegador debe ser compatible con for..of loop
+// y variables de alcance permitido en bucles for
+// también se pueden usar const y var
+
+for (let caracter of literable_matriz) {
+
+  const lista = document.createElement('li');
+
+  lista.textContent = caracter;
+
+  resultado_caracter.appendChild(lista);
+
+}
+
+
+var arr = ['a', 'b', 'c', 'd', 'e'];
+var eArr = arr[Symbol.iterator]();
+console.log(eArr.next().value); // a
+console.log(eArr.next().value); // b
+console.log(eArr.next().value); // c
+console.log(eArr.next().value); // d
+console.log(eArr.next().value); // e
+
+
+function logIterable(it) {
+  if (!(Symbol.iterator in Object.getPrototypeOf(it)
+      /* or "Symbol.iterator in Object.__proto__"
+         or "it[Symbol.iterator]" */
+    )) {
+    console.log(it, ' is not an iterable object...');
+    return;
+  }
+
+  var iterator = it[Symbol.iterator]();
+  // your browser must support for..of loop
+  // and let-scoped variables in for loops
+  // const and var could also be used
+  for (let letter of iterator) {
+    console.log(letter);
+  }
+}
+
+// Array
+logIterable(['a', 'b', 'c']);
+// a
+// b
+// c
+
+// string
+logIterable('abc');
+// a
+// b
+// c
+
+logIterable(123);
+// 123 " is not an iterable object..."
